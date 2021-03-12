17ea59f7cfcebf360980db8600855b0150c556e8
kieukhanhquan <kieukhanhquan@gmail.com>
2021-03-12 19:46:19 +0700

    c0

---
+++ sada/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  sada
+//
+//  Created by KIEU KHANH QUAN on 12/03/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
